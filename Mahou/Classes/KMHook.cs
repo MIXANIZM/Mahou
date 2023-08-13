35f64656ecf2812e20ff0d73d7d1340670159668
BladeMight <BladeMight@gmail.com>
2023-08-13 23:28:22 +0300

    :memo: Fallback to child window for WM_INPTUTLANGCHANGEREQUEST   certain windows have their own way of processing that message,   like 7-zip's gui which look like child windows but actually not

--- Mahou/Classes/KMHook.cs
+++ Mahou/Classes/KMHook.cs
@@ -3770,10 +3770,15 @@ namespace Mahou {
 				if (LayoutId == 0) {
 					Logging.Log("Layout change skipped, 0 is not layout.", 1);
 				} else {
-					// WM_INPUTLANCHANGEREQUEST should not be sent to non-root windows
-					var root = WinAPI.GetAncestor(hwnd, WinAPI.GetAncestorFlags.GetRootOwner);
-					Logging.Log("Root window: " + root + " focus: " +hwnd);
-					WinAPI.PostMessage(root, (int)WinAPI.WM_INPUTLANGCHANGEREQUEST, 0, LayoutId);
+					if (CompareLayouts(last, loc) && tries > 0) {
+						Logging.Log("Root layout switch failed, trying child window: " + hwnd);
+						WinAPI.PostMessage(hwnd, (int)WinAPI.WM_INPUTLANGCHANGEREQUEST, 0, LayoutId);
+					} else {
+						// WM_INPUTLANCHANGEREQUEST should not be sent to non-root windows
+						var root = WinAPI.GetAncestor(hwnd, WinAPI.GetAncestorFlags.GetRootOwner);
+						Logging.Log("Root window: " + root + " focus: " + hwnd);
+						WinAPI.PostMessage(root, (int)WinAPI.WM_INPUTLANGCHANGEREQUEST, 0, LayoutId);
+					}
 				}
 				Thread.Sleep(10);//Give some time to switch layout
 				tries++;
