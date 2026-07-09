d38383b6ecc475b6795ffbb6550944668dfa259b
MIXANIZM <94414361+MIXANIZM@users.noreply.github.com>
2026-07-10 02:12:25 +0300

    Harden native clipboard access

--- Mahou/Classes/NativeClipboard.cs
+++ Mahou/Classes/NativeClipboard.cs
@@ -1,36 +1,39 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace Mahou
 {
     public static class NativeClipboard
     {
         #region DLL Imports/Constants
-        [DllImport("user32.dll")]
+        [DllImport("user32.dll", SetLastError = true)]
         static extern IntPtr GetClipboardData(uint uFormat);
-        [DllImport("user32.dll")]
+        [DllImport("user32.dll", SetLastError = true)]
         static extern IntPtr SetClipboardData(uint uFormat, IntPtr hMem);
         [DllImport("user32.dll", SetLastError = true)]
         public static extern bool EmptyClipboard();
-        [DllImport("user32.dll")]
+        [DllImport("user32.dll", SetLastError = true)]
         static extern bool OpenClipboard(IntPtr hWndNewOwner);
         [DllImport("user32.dll", SetLastError = true)]
         static extern bool CloseClipboard();
-        [DllImport("user32.dll")]
+        [DllImport("user32.dll", SetLastError = true)]
         public static extern bool IsClipboardFormatAvailable(uint format);
-        [DllImport("kernel32.dll")]
+        [DllImport("kernel32.dll", SetLastError = true)]
         static extern IntPtr GlobalLock(IntPtr hMem);
-        [DllImport("kernel32.dll")]
+        [DllImport("kernel32.dll", SetLastError = true)]
         public static extern IntPtr GlobalUnlock(IntPtr hMem);
-        [DllImport("kernel32.dll")]
+        [DllImport("kernel32.dll", SetLastError = true)]
         public static extern IntPtr GlobalAlloc(uint uFlags, UIntPtr dwBytes);
-        [DllImport("kernel32.dll")]
+        [DllImport("kernel32.dll", SetLastError = true)]
         public static extern UIntPtr GlobalSize(IntPtr hMem);
         [DllImport("kernel32.dll")]
         static extern uint EnumClipboardFormats(uint format);
         public const uint GMEM_DDESHARE = 0x2000;
         public const uint GMEM_MOVEABLE = 0x2;
+        private const int ClipboardOpenAttempts = 50;
+        private const int ClipboardOpenDelayMs = 10;
         public enum uFormat
         {
             CF_TEXT = 1,
@@ -47,36 +50,52 @@ namespace Mahou
             CF_UNICODETEXT = 13
         }
         #endregion
+
         public static void Clear() // Clears Clipboard
         {
-            OpenClipboard(IntPtr.Zero);
-            EmptyClipboard();
-            CloseClipboard();
+            if (!TryOpenClipboard())
+                return;
+
+            try
+            {
+                EmptyClipboard();
+            }
+            finally
+            {
+                CloseClipboard();
+            }
         }
+
         public static string GetText() // Gets text data from clipboard
         {
             if (!IsClipboardFormatAvailable((uint)uFormat.CF_UNICODETEXT))
                 return null;
-            int Tries = 0;
-            var opened = false;
-            string data = null;
-            while (true)
+
+            if (!TryOpenClipboard())
+                return null;
+
+            IntPtr hGlobal = IntPtr.Zero;
+            IntPtr locked = IntPtr.Zero;
+            try
             {
-                ++Tries;
-                opened = OpenClipboard(IntPtr.Zero);
-                var hGlobal = GetClipboardData((uint)uFormat.CF_UNICODETEXT);
-                var lpwcstr = GlobalLock(hGlobal);
-                data = Marshal.PtrToStringUni(lpwcstr);
-                if (opened)
-                {
-                    GlobalUnlock(hGlobal);
-                    break;
-                }
-                System.Threading.Thread.Sleep(1);
+                hGlobal = GetClipboardData((uint)uFormat.CF_UNICODETEXT);
+                if (hGlobal == IntPtr.Zero)
+                    return null;
+
+                locked = GlobalLock(hGlobal);
+                if (locked == IntPtr.Zero)
+                    return null;
+
+                return Marshal.PtrToStringUni(locked);
             }
-            CloseClipboard();
-            return data;
+            finally
+            {
+                if (locked != IntPtr.Zero && hGlobal != IntPtr.Zero)
+                    GlobalUnlock(hGlobal);
+                CloseClipboard();
+            }
         }
+
         public static ClipboardData GetClipboardDatas() // Gets all clipboard datas, but only text-based datas supported...
         {
             var cd = new ClipboardData()
@@ -84,56 +103,97 @@ namespace Mahou
                 data = new List<byte[]>(),
                 format = new List<uint>()
             };
-            OpenClipboard(IntPtr.Zero);
-            foreach (var fmt in (uint[])Enum.GetValues(typeof(uFormat)))
+
+            if (!TryOpenClipboard())
+                return cd;
+
+            try
             {
-                IntPtr pos = GetClipboardData(fmt);
-                if (pos == IntPtr.Zero)
-                    continue;
-                UIntPtr lenght = GlobalSize(pos);
-                IntPtr gLock = GlobalLock(pos);
-                //Console.WriteLine(fmt + " is awaible in clipboard!!");
-                byte[] data;
-                if ((uint)lenght > 0)
+                foreach (var fmt in (uint[])Enum.GetValues(typeof(uFormat)))
                 {
-                    //Init a buffer which will contain the clipboard data
-                    data = new byte[(uint)lenght];
-                    //Console.WriteLine(lenght);
-                    int l = Convert.ToInt32(lenght.ToString());
-                    //Copy data from clipboard to our byte[] buffer
-                    Marshal.Copy(gLock, data, 0, l);
+                    IntPtr pos = GetClipboardData(fmt);
+                    if (pos == IntPtr.Zero)
+                        continue;
+
+                    UIntPtr length = GlobalSize(pos);
+                    var byteCount = length.ToUInt32();
+                    if (byteCount == 0)
+                        continue;
+
+                    IntPtr gLock = GlobalLock(pos);
+                    if (gLock == IntPtr.Zero)
+                        continue;
+
+                    try
+                    {
+                        byte[] data = new byte[byteCount];
+                        Marshal.Copy(gLock, data, 0, data.Length);
+                        cd.data.Add(data);
+                        cd.format.Add(fmt);
+                    }
+                    finally
+                    {
+                        GlobalUnlock(pos);
+                    }
                 }
-                else
-                {
-                    data = new byte[0];
-                }
-                cd.data.Add(data);
-                cd.format.Add(fmt);
             }
-            CloseClipboard();
+            finally
+            {
+                CloseClipboard();
+            }
+
             return cd;
         }
+
         public static void RestoreData(ClipboardData datas) // Places all datas to clipboard, but only text-based datas supported...
         {
-            OpenClipboard(IntPtr.Zero);
-            EmptyClipboard();
-            for (int i = 0; i != datas.data.Count; i++)
+            if (datas.data == null || datas.format == null || datas.data.Count != datas.format.Count)
+                return;
+
+            if (!TryOpenClipboard())
+                return;
+
+            try
             {
-                var data = datas.data[i];
-                //foreach (var d in data)
-                //{
-                //    Console.WriteLine("|"+d);
-                //}
-                //Console.WriteLine(data.GetLength(0));
-                IntPtr alloc = GlobalAlloc(GMEM_MOVEABLE | GMEM_DDESHARE, new UIntPtr(Convert.ToUInt32(data.GetLength(0))));
-                var glock = GlobalLock(alloc);
-                var fmt = datas.format[i];
-                Marshal.Copy(data, 0, glock, data.GetLength(0));
-                GlobalUnlock(alloc);
-                SetClipboardData(fmt, alloc);
+                EmptyClipboard();
+                for (int i = 0; i != datas.data.Count; i++)
+                {
+                    var data = datas.data[i];
+                    if (data == null || data.Length == 0)
+                        continue;
+
+                    IntPtr alloc = GlobalAlloc(GMEM_MOVEABLE | GMEM_DDESHARE, new UIntPtr(Convert.ToUInt32(data.Length)));
+                    if (alloc == IntPtr.Zero)
+                        continue;
+
+                    var glock = GlobalLock(alloc);
+                    if (glock == IntPtr.Zero)
+                        continue;
+
+                    var fmt = datas.format[i];
+                    Marshal.Copy(data, 0, glock, data.Length);
+                    GlobalUnlock(alloc);
+                    SetClipboardData(fmt, alloc);
+                }
             }
-            CloseClipboard();
+            finally
+            {
+                CloseClipboard();
+            }
         }
+
+        private static bool TryOpenClipboard()
+        {
+            for (int attempt = 0; attempt < ClipboardOpenAttempts; attempt++)
+            {
+                if (OpenClipboard(IntPtr.Zero))
+                    return true;
+
+                Thread.Sleep(ClipboardOpenDelayMs);
+            }
+            return false;
+        }
+
         public struct ClipboardData // Struct of List of byte[](data) and uint(data format)
         {
             public List<byte[]> data;
