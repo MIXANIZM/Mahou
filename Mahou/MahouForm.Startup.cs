4b09675d36ffe1f5b8d9fe6defa9be377b9f4d0e
MIXANIZM <94414361+MIXANIZM@users.noreply.github.com>
2026-07-10 17:27:13 +0300

    Replace dead repository link with MIXANIZM Mahou

--- Mahou/MahouForm.Startup.cs
+++ Mahou/MahouForm.Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace Mahou
@@ -12,6 +13,7 @@ namespace Mahou
             base.OnLoad(e);
             ApplySecurityPolicyUi();
             ReplaceLegacyApplyHandlers();
+            ReplaceLegacyRepositoryLink();
             RefreshStartupCheckboxFromRegistry();
         }
 
@@ -35,18 +37,34 @@ namespace Mahou
             btnOK.Click += SafeOk_Click;
         }
 
+        private void ReplaceLegacyRepositoryLink()
+        {
+            GitHubLink.LinkClicked -= GitHubLink_LinkClicked;
+            GitHubLink.LinkClicked -= SafeGitHubLink_LinkClicked;
+            GitHubLink.LinkClicked += SafeGitHubLink_LinkClicked;
+        }
+
+        private void SafeGitHubLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try { Process.Start("https://github.com/MIXANIZM/Mahou"); }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "MIXANIZM Mahou", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
         private void SafeApply_Click(object sender, EventArgs e)
         {
-            ApplyWithRegistryStartup(false);
+            ApplyWithRegistryStartup();
         }
 
         private void SafeOk_Click(object sender, EventArgs e)
         {
-            if (ApplyWithRegistryStartup(true))
+            if (ApplyWithRegistryStartup())
                 ToggleVisibility();
         }
 
-        private bool ApplyWithRegistryStartup(bool closing)
+        private bool ApplyWithRegistryStartup()
         {
             if (safeApplyActive)
                 return false;
